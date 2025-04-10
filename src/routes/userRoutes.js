414d4246e1b5bbfa9a579a3317879b353b47b7c4
Victor Costa <victorcosta.bsi@gmail.com>
2025-04-09 21:04:38 -0300

    criação de rotas

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,6 +11,22 @@ router.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../views') });
 });
 
+router.post('/store', (req, res) => {
+  res.sendFile('store.html', { root: path.join(__dirname, '../views') });
+});
+
+router.get('/index', (req, res) => {
+  res.sendFile('index.html', { root: path.join(__dirname, '../views') });
+});
+
+router.patch('/update/:id', (req, res) => {
+  res.sendFile('update.html', { root: path.join(__dirname, '../views') });
+});
+
+router.delete('/destroy/:id', (req, res) => {
+  res.sendFile('delete.html', { root: path.join(__dirname, '../views') });
+});
+
 router.post('/register', userController.registerUser);
 router.get('/index', userController.indexUser);
 router.put('/update/:id', userController.updateUser);
